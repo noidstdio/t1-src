43df266954bb5b9745ab251a4c38d081ed2a0a32
grant <80866912+atmossig@users.noreply.github.com>
2025-08-16 14:45:00 -0500

    Updated force include

---
+++ emptyfile.cpp
@@ -0,0 +1,6 @@
+// I am an empty file
+// so that I can access C++ settings
+// on VS.
+
+#if LYNN_MSVC
+#endif
---
+++ lynn/forceinclude-win64-release-msvc.h
@@ -0,0 +1,37 @@
+#ifndef __FORCEINCLUDE_WIN64_DEBUG__
+#define __FORCEINCLUDE_WIN64_DEBUG__
+
+#define _WIN64 	1
+#define _PC 	1
+#define _LINUX 	0
+#define _MACOS 	0
+
+#define LYNN_WIN64 	_WIN64
+#define LYNN_PC 	_PC
+#define LYNN_LINUX 	_LINUX
+#define LYNN_MACOS 	_MACOS
+
+#define _COMPILER_GCC 		0
+#define _COMPILER_MSVC 		1
+#define _COMPILER_CLANG 	0
+
+#define LYNN_GCC		_COMPILER_GCC
+#define LYNN_MSVC		_COMPILER_MSVC
+#define LYNN_CLANG		_COMPILER_CLANG
+
+#define _DEBUG 			0
+#define _RELEASE 		1
+#define _OPTIMIZED 		1
+#define _ASSERT 		0
+#define _DEV 			0
+#define _DEV_BANK 		0
+
+#define LYNN_DEBUG 				_DEBUG
+#define LYNN_RELEASE 			_RELEASE
+#define LYNN_OPTIMIZED 			_OPTIMIZED
+#define LYNN_ASSERT 			_ASSERT
+#define LYNN_DEV 				_DEV
+#define LYNN_BANK 				_DEV_BANK
+#define LYNN_DEV_BANK 			_DEV_BANK
+
+#endif // __FORCEINCLUDE_WIN64_DEBUG__
